502de003522ad3aa1eb00f5fcf86ddde9fb6b1af
devpatel2103 <devpatel2103@gmail.com>
2025-04-27 16:31:31 -0400

    updated .sql file

--- 336project.sql
+++ 336project.sql
@@ -18,27 +18,27 @@ USE `336project`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `aircrafts`
+-- Table structure for table `airline`
 --
 
-DROP TABLE IF EXISTS `aircrafts`;
+DROP TABLE IF EXISTS `airline`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `aircrafts` (
-  `aircraft_id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE `airline` (
+  `airline_id` varchar(5) NOT NULL,
   `airline` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`aircraft_id`)
+  PRIMARY KEY (`airline_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `aircrafts`
+-- Dumping data for table `airline`
 --
 
-LOCK TABLES `aircrafts` WRITE;
-/*!40000 ALTER TABLE `aircrafts` DISABLE KEYS */;
-INSERT INTO `aircrafts` VALUES (1,'United'),(2,'American Airlines'),(3,'Delta'),(4,'Southwest'),(5,'Emirates');
-/*!40000 ALTER TABLE `aircrafts` ENABLE KEYS */;
+LOCK TABLES `airline` WRITE;
+/*!40000 ALTER TABLE `airline` DISABLE KEYS */;
+INSERT INTO `airline` VALUES ('AA','American Airlines'),('DL','Delta'),('EK','Emirates'),('UA','United'),('WN','Southwest');
+/*!40000 ALTER TABLE `airline` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -49,11 +49,10 @@ DROP TABLE IF EXISTS `airports`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `airports` (
-  `airport_id` INT NOT NULL AUTO_INCREMENT,
-  `airport_code` varchar(3) DEFAULT NULL,
+  `airport_code` varchar(3) NOT NULL,
   `city` varchar(50) DEFAULT NULL,
   `state` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`airport_id`)
+  PRIMARY KEY (`airport_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -63,7 +62,7 @@ CREATE TABLE `airports` (
 
 LOCK TABLES `airports` WRITE;
 /*!40000 ALTER TABLE `airports` DISABLE KEYS */;
-INSERT INTO `airports` VALUES (1,'EWR','Newark','New Jersey'),(2,'RDU','Raleigh','North Carolina'),(3,'BOS','Boston','Massachusetts'),(4,'ORD','Chicago','Illinois');
+INSERT INTO `airports` VALUES ('BOS','Boston','Massachusetts'),('EWR','Newark','New Jersey'),('ORD','Chicago','Illinois'),('RDU','Raleigh','North Carolina');
 /*!40000 ALTER TABLE `airports` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -75,14 +74,14 @@ DROP TABLE IF EXISTS `flights`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `flights` (
-  `flight_id` INT NOT NULL AUTO_INCREMENT,
-  `aircraft_id` int DEFAULT NULL,
+  `flight_id` int NOT NULL AUTO_INCREMENT,
+  `airline_id` varchar(5) NOT NULL,
   `total_first_class_seats` int DEFAULT NULL,
   `total_business_seats` int DEFAULT NULL,
   `total_economy_seats` int DEFAULT NULL,
-  `sold_firstclass_seats` int DEFAULT 0,
-  `sold_business_seats` int DEFAULT 0,
-  `sold_economy_seats` int DEFAULT 0,
+  `sold_firstclass_seats` int DEFAULT '0',
+  `sold_business_seats` int DEFAULT '0',
+  `sold_economy_seats` int DEFAULT '0',
   `firstclass_price` int DEFAULT NULL,
   `business_price` int DEFAULT NULL,
   `economy_price` int DEFAULT NULL,
@@ -90,17 +89,17 @@ CREATE TABLE `flights` (
   `takeoff_time` datetime DEFAULT NULL,
   `landing_time` datetime DEFAULT NULL,
   `duration` int DEFAULT NULL,
-  `origin_airport_id` int DEFAULT NULL,
-  `destination_airport_id` int DEFAULT NULL,
-  `total_seats_sold` int DEFAULT 0,
+  `origin_airport_code` varchar(3) DEFAULT NULL,
+  `destination_airport_code` varchar(3) DEFAULT NULL,
+  `total_seats_sold` int DEFAULT '0',
   PRIMARY KEY (`flight_id`),
-  KEY `aircraft_id` (`aircraft_id`),
-  KEY `origin_airport_id` (`origin_airport_id`),
-  KEY `destination_airport_id` (`destination_airport_id`),
-  CONSTRAINT `flights_ibfk_1` FOREIGN KEY (`aircraft_id`) REFERENCES `aircrafts` (`aircraft_id`),
-  CONSTRAINT `flights_ibfk_2` FOREIGN KEY (`origin_airport_id`) REFERENCES `airports` (`airport_id`),
-  CONSTRAINT `flights_ibfk_3` FOREIGN KEY (`destination_airport_id`) REFERENCES `airports` (`airport_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `airline_id` (`airline_id`),
+  KEY `origin_airport_code` (`origin_airport_code`),
+  KEY `destination_airport_code` (`destination_airport_code`),
+  CONSTRAINT `flights_ibfk_1` FOREIGN KEY (`airline_id`) REFERENCES `airline` (`airline_id`),
+  CONSTRAINT `flights_ibfk_2` FOREIGN KEY (`origin_airport_code`) REFERENCES `airports` (`airport_code`),
+  CONSTRAINT `flights_ibfk_3` FOREIGN KEY (`destination_airport_code`) REFERENCES `airports` (`airport_code`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -109,7 +108,7 @@ CREATE TABLE `flights` (
 
 LOCK TABLES `flights` WRITE;
 /*!40000 ALTER TABLE `flights` DISABLE KEYS */;
-INSERT INTO `flights` VALUES (1,2,5,5,20,0,0,0,200,100,50,0,'2025-05-01 14:00:00','2025-05-01 18:30:00',270,4,1,0),(2,1,2,5,20,0,0,0,200,100,50,0,'2025-04-21 12:00:00','2025-04-21 16:00:00',240,1,2,0),(3,5,3,3,20,0,0,0,200,100,50,0,'2025-03-01 08:00:00','2025-03-01 12:00:00',240,3,2,0);
+INSERT INTO `flights` VALUES (1,'AA',5,5,20,0,0,0,200,100,50,0,'2025-05-01 14:00:00','2025-05-01 18:30:00',270,'ORD','EWR',0),(2,'UA',2,5,20,0,0,0,200,100,50,0,'2025-04-21 12:00:00','2025-04-21 16:00:00',240,'EWR','RDU',0),(3,'EK',3,3,20,0,0,0,200,100,50,0,'2025-03-01 08:00:00','2025-03-01 12:00:00',240,'BOS','RDU',0);
 /*!40000 ALTER TABLE `flights` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -146,29 +145,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-<<<<<<< HEAD
--- Dump completed on 2025-04-27  1:23:24
-=======
-
--- Airlines table
-CREATE TABLE `airlines` (
-  `airline_id` INT NOT NULL AUTO_INCREMENT,
-  `airline_name` VARCHAR(100) NOT NULL,
-  `airline_code` VARCHAR(10) NOT NULL,
-  PRIMARY KEY (`airline_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Customers table
-CREATE TABLE `customers` (
-  `customer_id` INT NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(100) NOT NULL,
-  `last_name` VARCHAR(100) NOT NULL,
-  `email` VARCHAR(100),
-  `phone_number` VARCHAR(15),
-  `address` VARCHAR(100) NOT NULL,
-  `username` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`customer_id`),
-  FOREIGN KEY (`username`) REFERENCES `users` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
->>>>>>> 89c909aa92755e150213fa24b31fcc3c291c5f65
+-- Dump completed on 2025-04-27 16:30:05

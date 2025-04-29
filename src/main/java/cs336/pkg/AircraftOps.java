303dd6c0fcd4f12be6f69cb8ec4accedc0c01afb
EKDrifter458 <98769437+EKDrifter458@users.noreply.github.com>
2025-04-28 20:12:44 -0400

    Update AircraftOps.java

--- src/main/java/cs336/pkg/AircraftOps.java
+++ src/main/java/cs336/pkg/AircraftOps.java
@@ -9,12 +9,21 @@ public class AircraftOps {
 
     // Add new aircraft
     public void addAircraft(List<Aircraft> aircraftList, Aircraft aircraft) {
+        if (aircraft == null) {
+            System.out.println("Cannot add null aircraft.");
+            return;
+        }
         aircraftList.add(aircraft);
         System.out.println("Aircraft added: " + aircraft);
     }
 
     // Edit existing aircraft
     public void editAircraft(List<Aircraft> aircraftList, Aircraft oldAircraft, Aircraft newAircraft) {
+        if (oldAircraft == null || newAircraft == null) {
+            System.out.println("Invalid aircraft provided.");
+            return;
+        }
+
         int index = aircraftList.indexOf(oldAircraft);
         if (index != -1) {
             aircraftList.set(index, newAircraft);
@@ -26,6 +35,11 @@ public class AircraftOps {
 
     // Delete aircraft
     public void deleteAircraft(List<Aircraft> aircraftList, Aircraft aircraft) {
+        if (aircraft == null) {
+            System.out.println("Cannot remove null aircraft.");
+            return;
+        }
+
         if (aircraftList.remove(aircraft)) {
             System.out.println("Aircraft removed: " + aircraft);
         } else {
@@ -35,12 +49,21 @@ public class AircraftOps {
 
     // Add airport
     public void addAirport(List<Airport> airportList, Airport airport) {
+        if (airport == null) {
+            System.out.println("Cannot add null airport.");
+            return;
+        }
         airportList.add(airport);
         System.out.println("Airport added: " + airport);
     }
 
     // Edit airport
     public void editAirport(List<Airport> airportList, Airport oldAirport, Airport newAirport) {
+        if (oldAirport == null || newAirport == null) {
+            System.out.println("Invalid airport provided.");
+            return;
+        }
+
         int index = airportList.indexOf(oldAirport);
         if (index != -1) {
             airportList.set(index, newAirport);
@@ -52,6 +75,11 @@ public class AircraftOps {
 
     // Delete airport
     public void deleteAirport(List<Airport> airportList, Airport airport) {
+        if (airport == null) {
+            System.out.println("Cannot remove null airport.");
+            return;
+        }
+
         if (airportList.remove(airport)) {
             System.out.println("Airport removed: " + airport);
         } else {
@@ -61,12 +89,21 @@ public class AircraftOps {
 
     // Add flight
     public void addFlight(List<Flight> flightList, Flight flight) {
+        if (flight == null) {
+            System.out.println("Cannot add null flight.");
+            return;
+        }
         flightList.add(flight);
         System.out.println("Flight added: " + flight);
     }
 
     // Edit flight
     public void editFlight(List<Flight> flightList, Flight oldFlight, Flight newFlight) {
+        if (oldFlight == null || newFlight == null) {
+            System.out.println("Invalid flight provided.");
+            return;
+        }
+
         int index = flightList.indexOf(oldFlight);
         if (index != -1) {
             flightList.set(index, newFlight);
@@ -78,6 +115,11 @@ public class AircraftOps {
 
     // Delete flight
     public void deleteFlight(List<Flight> flightList, Flight flight) {
+        if (flight == null) {
+            System.out.println("Cannot remove null flight.");
+            return;
+        }
+
         if (flightList.remove(flight)) {
             System.out.println("Flight removed: " + flight);
         } else {

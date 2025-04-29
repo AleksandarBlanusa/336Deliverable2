6796e5ea05582a93cae109debf455f1b8434cba5
EKDrifter458 <98769437+EKDrifter458@users.noreply.github.com>
2025-04-28 21:20:17 -0400

    Update CustomerRep.java

--- src/main/java/cs336/pkg/CustomerRep.java
+++ src/main/java/cs336/pkg/CustomerRep.java
@@ -1,7 +1,7 @@
 package cs336.pkg;
 
 import cs336.pkg.Airport;
-import cs336.pkg.Customer;
+import cs336.pkg.User;
 import cs336.pkg.Flight;
 
 import java.util.ArrayList;
@@ -9,52 +9,52 @@ import java.util.List;
 
 public class CustomerRep {
 
-    private List<Customer> waitingList = new ArrayList<>();
+    private List<User> waitingList = new ArrayList<>();
 
     // Make flight reservation
-    public boolean makeReservation(Flight flight, Customer customer) {
-        if (flight.addCustomer(customer)) {
-            System.out.println("Reservation made for " + customer.getFirstName() + " on Flight ID: " + flight.getFlightId());
+    public boolean makeReservation(Flight flight, User user) {
+        if (flight.addUser(user)) {
+            System.out.println("Reservation made for " + user.getFirstName() + " on Flight ID: " + flight.getFlightId());
             return true;
         } else {
             System.out.println("Flight is fully booked. Adding to waiting list...");
-            addToWaitingList(customer);
+            addToWaitingList(user);
             return false;
         }
     }
 
     // Add to waiting list if flight is fully booked
-    public void addToWaitingList(Customer customer) {
-        waitingList.add(customer);
+    public void addToWaitingList(User user) {
+        waitingList.add(user);
     }
 
     // Edit reservation
-    public void editReservation(Flight flight, Customer oldCustomer, Customer newCustomer) {
-        List<Customer> customers = flight.getCustomers();
+    public void editReservation(Flight flight, User oldUser, User newUser) {
+        List<User> users = flight.getUsers();
         
-        for (int i = 0; i < customers.size(); i++) {
-            if (customers.get(i).getCustomerId() == oldCustomer.getCustomerId()) {
-                customers.set(i, newCustomer);
-                System.out.println("Reservation updated for Customer ID: " + oldCustomer.getCustomerId());
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getUserId() == oldUser.getUserId()) {
+                users.set(i, newUser);
+                System.out.println("Reservation updated for User ID: " + oldUser.getUserId());
                 return;
             }
         }
-        System.out.println("Customer not found.");
+        System.out.println("User not found.");
     }
 
     // Cancel reservation
-    public void cancelReservation(Flight flight, Customer customer) {
-        if (flight.getCustomers().remove(customer)) {
-            System.out.println("Reservation cancelled for Customer ID: " + customer.getCustomerId());
-            // Check if any customers are waiting and move one to the flight
+    public void cancelReservation(Flight flight, User user) {
+        if (flight.getUsers().remove(user)) {
+            System.out.println("Reservation cancelled for User ID: " + user.getUserId());
+            // Check if any users are waiting and move one to the flight
             if (!waitingList.isEmpty()) {
-                Customer nextCustomer = waitingList.remove(0); // Get the first customer in the waiting list
-                if (flight.addCustomer(nextCustomer)) {
-                    System.out.println("Customer from waiting list added: " + nextCustomer.getFirstName());
+                User nextUser = waitingList.remove(0); // Get the first user in the waiting list
+                if (flight.addUser(nextUser)) {
+                    System.out.println("User from waiting list added: " + nextUser.getFirstName());
                 }
             }
         } else {
-            System.out.println("Customer not found in flight.");
+            System.out.println("User not found in flight.");
         }
     }
 
@@ -84,7 +84,7 @@ public class CustomerRep {
     }
 
     // Get waiting list for flights
-    public List<Customer> getWaitingList() {
+    public List<User> getWaitingList() {
         return waitingList;
     }
 }

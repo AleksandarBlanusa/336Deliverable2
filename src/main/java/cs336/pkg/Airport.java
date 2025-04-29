cfd4b9bd892638c2f78a929732a33d13753cfe71
EKDrifter458 <98769437+EKDrifter458@users.noreply.github.com>
2025-04-29 01:05:16 -0400

    Update Airport.java

--- src/main/java/cs336/pkg/Airport.java
+++ src/main/java/cs336/pkg/Airport.java
@@ -1,46 +1,39 @@
 package cs336.pkg;
+
 public class Airport {
     private int airportId;
     private String airportCode;
-    private String airportName;
     private String city;
-    private String country;
-    
-    // Constructors
+    private String state;
+
     public Airport() {}
-    
-    public Airport(int airportId, String airportCode, String airportName, String city, String country) {
+
+    public Airport(int airportId, String airportCode, String city, String state) {
         this.airportId = airportId;
         this.airportCode = airportCode;
-        this.airportName = airportName;
         this.city = city;
-        this.country = country;
+        this.state = state;
     }
-    
-    // Getters and Setters
+
     public int getAirportId() { return airportId; }
     public void setAirportId(int airportId) { this.airportId = airportId; }
-    
+
     public String getAirportCode() { return airportCode; }
     public void setAirportCode(String airportCode) { this.airportCode = airportCode; }
-    
-    public String getAirportName() { return airportName; }
-    public void setAirportName(String airportName) { this.airportName = airportName; }
-    
+
     public String getCity() { return city; }
     public void setCity(String city) { this.city = city; }
-    
-    public String getCountry() { return country; }
-    public void setCountry(String country) { this.country = country; }
-    
+
+    public String getState() { return state; }
+    public void setState(String state) { this.state = state; }
+
     @Override
     public String toString() {
         return "Airport{" +
                 "airportId=" + airportId +
                 ", airportCode='" + airportCode + '\'' +
-                ", airportName='" + airportName + '\'' +
                 ", city='" + city + '\'' +
-                ", country='" + country + '\'' +
+                ", state='" + state + '\'' +
                 '}';
     }
 }
